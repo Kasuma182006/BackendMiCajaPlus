e751dafba5d2c10b3f2ce3bff0bbc5b215a65c53
andres <andresvelasquez182006@gmail.com>
2026-03-02 22:20:53 -0500

    Descarga de productos por ventas en inventario

--- models/inventario.py
+++ models/inventario.py
@@ -2,15 +2,41 @@ from conexion import obtenerConexion
 
 class Inventario:
 
-    def cargarInventario(self,idTendero):
-
+    def buscarProducto(self,nombre,presentacion):
         conexion=obtenerConexion()
-        cursor=conexion.cursor(dictionary=True)
-        cursor.execute("""Select inventario.idInventario,inventario.idProductos,inventario.cantidad,inventario.valorVenta,inventario.valorCompra, productos.idCategorias, productos.nombre, productos.presentacion FROM inventario INNER JOIN productos ON inventario.idProductos = productos.idProductos WHERE inventario.idTendero = %s """, (idTendero,))
-        resultado = cursor.fetchall();
+        cursor=conexion.cursor(dictionary= True)
+        cursor.execute("""Select idProductos FROM productos where nombre = %s AND presentacion = %s""",(nombre,presentacion))
+        resultado= cursor.fetchone()
         cursor.close()
         conexion.close()
-        return resultado;
+        print(resultado)
+        return resultado
+   
+    def buscarInventario(self,idTendero,idProducto):
+        conexion=obtenerConexion()
+        cursor=conexion.cursor(dictionary=True)
+        cursor.execute("""Select idInventario, cantidad From inventario where idTendero = %s AND idProductos = %s""", (idTendero,idProducto) )
+        resultado= cursor.fetchone()
+        cursor.close()
+        conexion.close()
+        print(resultado)
+        return resultado
+        
+    
+
+    def actualizarProducto(self, producto):
+        conexion=obtenerConexion()
+        cursor=conexion.cursor()
+        cursor.execute("""UPDATE inventario SET cantidad = %s where idInventario = %s""",(producto["cantidad"], producto["idInventario"]) )
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+    
+
+
+
+
+
 
 
 
--- routes/inventario.py
+++ routes/inventario.py
@@ -5,12 +5,29 @@ from models.inventario import Inventario
 
 def cargar_rutas_inventario(app):
 
-    @app.route("/cargar_inventario", methods=["GET"])
-    def cargarInventario():
-        try:
-            idTendero = request.args.get("idTendero")
-            inventario = Inventario()
-            datosInventario = inventario.cargarInventario(idTendero)
-            return jsonify(datosInventario);
-        except Exception as e:
-            return jsonify({"error": str(e)})
+    @app.route("/descargaProductos", methods=["POST"])
+    def operacionesProductos():
+        producto = request.get_json()
+        inventarioTendero = Inventario()
+        
+        if not producto:
+            return jsonify({"Error": "Datos incompletos "}), 400
+        else: 
+            try: 
+                idProducto = inventarioTendero.buscarProducto( producto.get("nombre"), producto.get("presentacion"))
+                productoInventario = inventarioTendero.buscarInventario(producto.get("idTendero"), idProducto.get("idProductos"))
+            except:
+                return jsonify({"error": "error en el servidor"}),500
+            
+        if not productoInventario: 
+            return jsonify({"error" "No se ha encontrado el producto en inventario"})
+
+        if productoInventario["cantidad"] >= producto["cantidad"]:
+           productoInventario["cantidad"] = productoInventario["cantidad"] - producto["cantidad"]
+           try:
+                inventarioTendero.actualizarProducto(productoInventario)
+                return jsonify({"succesful": "Productos descontados correctamente"}),200
+           except:
+               return jsonify ({"Error": "Error en al procesar"}),500
+        else :  
+            return jsonify({"Error": "La cantidad de productos en stok es menor a la cantidad de productos vendidos"})          
