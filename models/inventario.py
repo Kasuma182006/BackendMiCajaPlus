a44ebef0f3fc9a155133744ed37c4b1a11db074b
andres <andresvelasquez182006@gmail.com>
2026-03-15 11:57:45 -0500

    modulo editar productos

--- models/inventario.py
+++ models/inventario.py
@@ -1,21 +1,26 @@
 from conexion import obtenerConexion
 
 class Inventario():
-    def buscarProducto(self,producto):
+    def buscarProducto(self,idTendero,nombre,presentacion):
 
         conexion=obtenerConexion()
         cursor=conexion.cursor(dictionary=True)
-        cursor.execute("""SELECT idInventario, cantidad FROM inventario WHERE idTendero = %s AND nombreProducto = %s AND presentacion = %s """,(producto.get("idTendero"),producto.get("nombre"),producto.get("presentacion")))
+        cursor.execute("""SELECT idInventario, cantidad,valorCompra FROM inventario WHERE idTendero = %s AND nombreProducto = %s AND presentacion = %s """,(idTendero,nombre,presentacion))
         resultado=cursor.fetchone()
         cursor.close()
         conexion.close()
         print(f"resultado de busquda = {resultado}")
         return resultado
     
-    def actualizarUnidades(self,producto):
+    def actualizarUnidades(self,producto,operacion):
         conexion=obtenerConexion()
         cursor=conexion.cursor()
-        cursor.execute("""UPDATE inventario SET cantidad = %s where idInventario = %s """, (producto.get("cantidad"),producto.get("idInventario")))
+        print(operacion)
+        if operacion == "descontar":
+            cursor.execute("""UPDATE inventario SET cantidad = %s where idInventario = %s """, (producto.get("cantidad"),producto.get("idInventario")))
+        else:
+            print(producto.get("valorCompra"))
+            cursor.execute("""UPDATE inventario SET cantidad = %s, valorCompra = %s where idInventario = %s """, (producto.get("cantidad"),producto.get("valorCompra"),producto.get("idInventario")))
         conexion.commit()
         cursor.close()
         conexion.close()
@@ -54,6 +59,26 @@ class Inventario():
         conexion=obtenerConexion()
         cursor=conexion.cursor()
         cursor.execute("""INSERT INTO productos(nombre,presentacion,idCategorias) VALUES(%s,%s,%s)""",(catalogo.get("nombre"), catalogo.get("presentacion"),catalogo.get("idCategoria")))
+        conexion.commit()
+        cursor.close()
+        conexion.close()
+
+    def sugerirProductos(self,producto):
+        conexion=obtenerConexion()
+        cursor=conexion.cursor(dictionary=True)
+        nombre = f"%{producto.get("nombre")}%"
+        cursor.execute("""Select idInventario,nombreProducto,presentacion,cantidad,valorVenta,valorCompra from inventario where idTendero = %s AND nombreProducto LIKE %s""",(producto.get("idTendero"),nombre))
+        resultado=cursor.fetchall()
+        cursor.close()
+        conexion.close()
+        print(f"resultado de busquda = {resultado}")
+        return resultado
+
+
+    def editarProducto(self,producto):
+        conexion=obtenerConexion()
+        cursor=conexion.cursor()
+        cursor.execute("""UPDATE inventario SET cantidad = %s,valorVenta = %s ,valorCompra = %s ,nombreProducto = %s ,presentacion = %s ,nombreCategoria = %s WHERE idInventario = %s""" ,(producto.get("cantidad"),producto.get("valorVenta"),producto.get("valorCompra"),producto.get("nombreProducto"),producto.get("presentacion"),producto.get("nombreCategoria"),producto.get("idInventario")))
         conexion.commit()
         cursor.close()
         conexion.close()
--- routes/inventario.py
+++ routes/inventario.py
@@ -12,7 +12,7 @@ def cargar_rutas_inventario(app):
         try:
             
             print(f"DEBUG: Buscando producto en BD: {producto.get('nombre')}")
-            productoInventario = inventario.buscarProducto(producto)
+            productoInventario = inventario.buscarProducto(producto.get("idTendero"),producto.get("nombre"),producto.get("presentacion"))
             
             if not productoInventario:
                 return jsonify({"error": "No se han encontrado coincidencias del producto"}), 404
@@ -31,11 +31,12 @@ def cargar_rutas_inventario(app):
                 
             else: 
                 productoInventario["cantidad"] = cantidad_actual + cantidad_solicitada
+                productoInventario["valorCompra"] = producto.get("valorCompra")
                 mensaje_success = "Se han agregado las unidades correctamente"
 
             
             print(f"DEBUG: Intentando actualizar BD con nueva cantidad: {productoInventario['cantidad']}")
-            inventario.actualizarUnidades(productoInventario)
+            inventario.actualizarUnidades(productoInventario,producto.get("operacion"))
             
             
             return jsonify({"status": "success", "message": mensaje_success}), 200
@@ -50,7 +51,7 @@ def cargar_rutas_inventario(app):
     def categorias():
         inventario= Inventario()
         try:
-            print("obteniendo categorias")
+            print("Obteniendo categorias..")
             categorias = inventario.categorias()
         except Exception as e:
             print(f"Error en la consulta {e}")
@@ -62,7 +63,7 @@ def cargar_rutas_inventario(app):
     def catalogo():
         inventario = Inventario()
         try: 
-            print("obteniendo catalago")
+            print("Obteniendo catalago...")
             listaCatalogos = inventario.catalogo()
         except Exception as e:
             print(e)
@@ -78,10 +79,13 @@ def cargar_rutas_inventario(app):
         inventario = Inventario()
         print(productoNuevo)
         try:
-            verificación = inventario.buscarProducto(productoNuevo)
-            if not verificación: 
+            verificación = inventario.buscarProducto(productoNuevo.get("idTendero"),productoNuevo.get("nombre"),productoNuevo.get("presentacion"))
+            if not verificación:
+                    print("DEBUG: Agregando productos : INSERT EN 1 TABLA") 
                     inventario.agregarProducto(productoNuevo)
-                    if productoNuevo.get("operacion") == 1: #1 significa que el tendero creo un producto sin elegir en el catalogo
+                    
+                    if productoNuevo.get("operacion") == 1: #1 significa que el tendero creo un producto sin elegir en el catalogo por lo tanto se inserta en la tabla del catalogo
+                        print("DEBUG: agregando producto: INSERT EN 2 TABLAS")
                         inventario.agregarCatalogo(productoNuevo)
             else: 
                 return jsonify({"Error": "El producto ya ha sido creado, elige un nombre o una presentación diferente"}),500
@@ -90,6 +94,45 @@ def cargar_rutas_inventario(app):
                 print(f"ERROR EN EL SERVIDOR, detalle : {e}")
                 return jsonify({"Error": f"Ha ocurrido un error en el servidor: {e}"}), 500  
         
-        return jsonify({"succesul":"El producto se ha creado con éxito"})
+        return jsonify({"succesful":"El producto se ha creado con éxito"}),200
 
 
+    @app.route("/sugerirProductos",methods = ["POST"])
+    def sugerirProductos():
+        inventario = Inventario()
+        productos = request.get_json()
+        try:
+            print("DEBUG: buscando Productos...")
+            listaProductos = inventario.sugerirProductos(productos)
+        except Exception as e:
+            print(f"Ha ocurrido un error Crítico en el servidor {e}")
+            return jsonify({"Error": f"Ha ocurrido un error Crítico en el servidor {e}"}),500
+
+        return jsonify(listaProductos) 
+    
+
+    @app.route("/editarProducto",methods= ["POST"])
+    def editarProducto():
+        producto = request.get_json()
+        inventario = Inventario()
+
+        try:
+            print(f"{producto.get("idTendero")},{producto.get("nombreProducto")},{producto.get("presentacion")}")
+            print("DEBUG: Verificando si hay productos con el mismo nombre y presentación")
+            productoInventario = inventario.buscarProducto(producto.get("idTendero"),producto.get("nombreProducto"),producto.get("presentacion"))
+
+            if not productoInventario:
+                print("DEBUG: Editando Producto...")
+                inventario.editarProducto(producto)
+            else:
+                if productoInventario.get("idInventario") == producto.get("idInventario"):
+                    print("DEBUG: Editando Producto ...")
+                    inventario.editarProducto(producto)
+                else:
+                    return jsonify({"Error": "Error al editar, ya tienes un producto con el mismo nombre y presentación"}),409
+
+        except Exception as e:
+            print(f"Error en consulta, {e}")
+            return jsonify({"error":f"Error en la base de datos, {e}"}),500        
+
+        return jsonify({"Succesful": "El producto se ha editado con éxito"}),200
